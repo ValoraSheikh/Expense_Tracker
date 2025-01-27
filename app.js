f94f2e5d8b5bf874ea3510dac8896e43bea6d4cd
Aman Sheikh <haarishsheikh04@gmail.com>
2025-01-27 10:29:19 +0530

    Added local storage to expense tracker

--- app.js
+++ app.js
@@ -3,44 +3,70 @@ const expenseName = document.querySelector('#name');
 const purchaseDate = document.querySelector('#date');
 const purchaseAmt = document.querySelector('#amount');
 const table = document.querySelector('table');
-let deleteTask;
+
+
+let expenses = JSON.parse(localStorage.getItem('expense')) || [];
+
+expenses.forEach(element => {
+    renderExpense(element)
+});
 
 function addNewExpense() {
-
     
     if (expenseName.value == "" || purchaseDate.value == "" || purchaseAmt == "") {
         alert(`First Enter the complete details`);
         return;
     }
 
-    let newExpense = document.createElement('tr');
-    table.append(newExpense);
+    let expenseObj = {
+        name: expenseName.value,
+        date: purchaseDate.value,
+        amount: purchaseAmt.value
+    }
 
-    let task = document.createElement('td');
-    task.textContent = `${expenseName.value}`
-    newExpense.append(task);
-
-    let taskDate = document.createElement('td');
-    taskDate.textContent = `${purchaseDate.value}`
-    newExpense.append(taskDate);
-
-    let taskAmt = document.createElement('td');
-    taskAmt.textContent = `$${purchaseAmt.value}`
-    newExpense.append(taskAmt);
-
-    deleteTask = document.createElement('td');
-    deleteTask.classList.add('deleteCell')
-    deleteTask.innerHTML = `<Button class="btn" >X</Button>`;
-    newExpense.append(deleteTask);
-
-    deleteTask.addEventListener('click', function () {
-        newExpense.remove();
-    })
+    expenses.push(expenseObj);
+    renderExpense(expenseObj);
+    saveExpenseToLS();
 
     expenseName.value = "";
     purchaseDate.value = ""; // type is date
     purchaseAmt.value = ""; // type is number
+}
 
+function renderExpense(expenseObj) {
+    let newExpense = document.createElement('tr');
+    table.append(newExpense);
+
+    let task = document.createElement('td');
+    task.textContent = `${expenseObj.name}`
+    newExpense.append(task);
+
+    let taskDate = document.createElement('td');
+    taskDate.textContent = `${expenseObj.date}`
+    newExpense.append(taskDate);
+
+    let taskAmt = document.createElement('td');
+    taskAmt.textContent = `$${expenseObj.amount}`
+    newExpense.append(taskAmt);
+
+    let deleteTask = document.createElement('td');
+    deleteTask.classList.add('deleteCell')
+    deleteTask.innerHTML = `<Button class="btn" >X</Button>`;
+    newExpense.append(deleteTask);
+
+    deleteTask.addEventListener('click', function (e) {
+
+        expenses = expenses.filter(element => element.name !== expenseObj.name)
+        console.log(expenses);
+        
+        newExpense.remove();
+        saveExpenseToLS();
+    })
+
+}
+
+function saveExpenseToLS() {
+    localStorage.setItem('expense', JSON.stringify(expenses));
 }
 
 addExpense.addEventListener('click', addNewExpense);
